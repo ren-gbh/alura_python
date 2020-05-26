58d48a97d364495399717a69316487c5c2dce31c
ren-gbh <ren.gbh@gmail.com>
2020-05-26 11:01:07 -0300

    1.5.4 - Finishing iteration and loop

--- parte1/parte1/jogos/adivinhacao.py
+++ parte1/parte1/jogos/adivinhacao.py
@@ -8,9 +8,12 @@ rodada = 1
 
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    chute_str = input("Digite o seu número: ")
+    chute_str = input("Digite o seu número entre 1 e 100: ")
     print("Você digitou: ", chute_str)
     chute = int(chute_str)
+
+    if (chute < 1 or chute > 100):
+        print ("Você deve digitar um número entre 1 e 100!")
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
