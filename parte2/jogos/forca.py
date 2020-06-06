8a76cbced81efffd953aeaa1ccfbc64d9a512a06
ren-gbh <ren.gbh@gmail.com>
2020-06-06 18:37:10 -0300

    2.1.5 - Game loop

---
+++ parte2/jogos/forca.py
@@ -0,0 +1,17 @@
+def jogar():
+
+    print("***************************")
+    print("Bem vindo ao jogo de Forca!")
+    print("***************************")
+
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while (not acertou and not enforcou):
+        print("Jogando...")
+
+    print("Fim do jogo")
+
+if (__name__ == "__main__"):
+    jogar()
