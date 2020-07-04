6d26bbcf6fa8c562c580e77573ecbff11c57f51c
ren-gbh <ren.gbh@gmail.com>
2020-07-04 15:59:55 -0300

    2.6.8 - Choosing a word

--- parte2/jogos/forca.py
+++ parte2/jogos/forca.py
@@ -1,10 +1,18 @@
+import random
+
 def jogar():
 
     print("***************************")
     print("Bem vindo ao jogo de Forca!")
     print("***************************")
 
-    palavra_secreta = "banana".upper()
+    arquivo = open("palavras.txt", "r")
+    palavras = [palavra.strip() for palavra in arquivo]
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras));
+
+    palavra_secreta = palavras[numero].upper()
     letras_acertadas = ["_" for letra in palavra_secreta]
     enforcou = False
     acertou = False
