31623918594efdd90820d42cf80d555d6cd89393
ren-gbh <ren.gbh@gmail.com>
2020-07-07 19:00:38 -0300

    3.1.3 - Data and Behavior
    Include functions deposita, saca and extrato on teste

--- parte3/oo/teste.py
+++ parte3/oo/teste.py
@@ -1,2 +1,11 @@
 def cria_conta(numero, titular, saldo, limite):
     return { "numero": numero, "titular": titular, "saldo": saldo, "limite": limite }
+
+def deposita(conta, valor):
+    conta["saldo"] += valor
+
+def saca(conta, valor):
+    conta["saldo"] -= valor
+
+def extrato(conta):
+    print("Saldo é {}".format(conta["saldo"]))
