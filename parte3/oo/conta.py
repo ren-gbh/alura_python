6624f3d492d4531546ef606ff6ad5e1d5bbce942
ren-gbh <ren.gbh@gmail.com>
2020-07-07 19:17:40 -0300

    3.2.3 - Constructor

--- parte3/oo/conta.py
+++ parte3/oo/conta.py
@@ -1,2 +1,7 @@
 class Conta:
-    pass
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo objeto...{}".format(self))
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
