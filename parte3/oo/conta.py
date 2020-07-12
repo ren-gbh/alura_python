5c2da2d62a54ededcbde727115d47d6ed9c9738e
ren-gbh <ren.gbh@gmail.com>
2020-07-12 16:16:15 -0300

    3.6.1 - Private methods

--- parte3/oo/conta.py
+++ parte3/oo/conta.py
@@ -29,7 +29,14 @@ class Conta:
         print("Saldo é {} do titular {}".format(self.__saldo, self.__titular))
 
     def saca(self, valor):
-        self.__saldo -= valor
+        if (self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("O valor {} passou o limite".format(valor))
+
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
 
     def transfere(self, valor, destino):
         self.saca(valor)
