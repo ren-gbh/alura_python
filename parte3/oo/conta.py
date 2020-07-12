e9dc88c7f2bb2df64e349ed0a5044b6c006aac19
ren-gbh <ren.gbh@gmail.com>
2020-07-12 15:12:03 -0300

    3.4.1 - Private attributes

--- parte3/oo/conta.py
+++ parte3/oo/conta.py
@@ -1,16 +1,16 @@
 class Conta:
     def __init__(self, numero, titular, saldo, limite):
         print("Construindo objeto...{}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def extrato(self):
-        print("Saldo é {} do titular {}".format(self.saldo, self.titular))
+        print("Saldo é {} do titular {}".format(self.__saldo, self.__titular))
 
     def saca(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
