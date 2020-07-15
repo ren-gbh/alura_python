f072c07ad8632d6f78826971ca8d99a7a0dad192
ren-gbh <ren.gbh@gmail.com>
2020-07-14 21:24:58 -0300

    4.1.4 - Adding attributes and methods

--- parte4/python3oo2/Modelo.py
+++ parte4/python3oo2/Modelo.py
@@ -1,17 +1,35 @@
 class Filme:
     def __init__(self, nome, ano, duracao):
-        self.nome = nome
+        self.nome = nome.title()
         self.ano = ano
         self.duracao = duracao
+        self.likes = 0
+
+    def dar_like(self):
+        self.likes += 1
 
 class Serie:
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome
+        self.nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
+        self.likes = 0
+
+    def dar_like(self):
+        self.likes += 1
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao}')
+vingadores.dar_like()
+print(f'Nome: {vingadores.nome} - '
+      f'Ano: {vingadores.ano} - '
+      f'Duração: {vingadores.duracao} - '
+      f'Likes: {vingadores.likes}')
 
 atlanta = Serie('atlanta', 2018, 2)
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas}')
+atlanta.nome = 'atlanta'
+atlanta.dar_like()
+atlanta.dar_like()
+print(f'Nome: {atlanta.nome} - '
+      f'Ano: {atlanta.ano} - '
+      f'Temporadas: {atlanta.temporadas} - '
+      f'Likes: {atlanta.likes}')
