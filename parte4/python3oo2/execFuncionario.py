b82d4527941a34ad5199e7ebd1e831563f35780e
ren-gbh <ren.gbh@gmail.com>
2020-07-16 19:35:25 -0300

    4.6.7 - Hands on
    Fixes execFuncionario class

--- parte4/python3oo2/execFuncionario.py
+++ parte4/python3oo2/execFuncionario.py
@@ -1,4 +1,11 @@
+from junior import Junior
 from senior import Senior
 
+jose = Junior('José')
+jose.busca_perguntas_sem_resposta()
+
 luan = Senior('Luan')
+luan.busca_perguntas_sem_resposta()
+luan.busca_cursos_do_mes()
+luan.mostrar_tarefas()
 print(luan)
