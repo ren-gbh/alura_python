71afa5f238048e7099df20e32aa1896443122c20
ren-gbh <ren.gbh@gmail.com>
2020-07-16 02:56:49 -0300

    4.3.1 - Polymorphism

--- parte4/python3oo2/modelo.py
+++ parte4/python3oo2/modelo.py
@@ -14,9 +14,14 @@ class Serie(Programa):
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 vingadores.dar_like()
-print(f'{vingadores.nome} - {vingadores.duracao} - {vingadores.likes}')
+print(f'{vingadores.nome} - {vingadores.duracao}: {vingadores.likes}')
 
 atlanta = Serie('atlanta', 2018, 2)
 atlanta.dar_like()
 atlanta.dar_like()
-print(f'{atlanta.nome} - {atlanta.temporadas} - {atlanta.likes}')
+print(f'{atlanta.nome} - {atlanta.temporadas}: {atlanta.likes}')
+
+filmes_e_series = [vingadores, atlanta]
+for programa in filmes_e_series:
+    detalhes = programa.duracao if hasattr(programa, 'duracao') else programa.temporadas
+    print(f'{programa.nome} - {detalhes} D -  {programa.likes}')
