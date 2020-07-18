6caeb79007243c8bb813eef17bd0146653267e0a
ren-gbh <ren.gbh@gmail.com>
2020-07-18 12:21:43 -0300

    5.2.3 - Building more methods

--- parte5-string/python/extratorArgumentosUrl.py
+++ parte5-string/python/extratorArgumentosUrl.py
@@ -12,3 +12,14 @@ class ExtratorArgumentosUrl:
             return True
         else:
             return False
+
+    def extrai_argumentos(self):
+        indice_inicial_moeda_origem = self.url.find("=") + 1
+        indice_final_moeda_origem = self.url.find("&")
+
+        indice_inicial_moeda_destino = self.url.find("=", 15) + 1
+
+        moeda_origem = self.url[indice_inicial_moeda_origem:indice_final_moeda_origem]
+        moeda_destino = self.url[indice_inicial_moeda_destino:]
+
+        return moeda_origem, moeda_destino
--- parte5-string/python/main.py
+++ parte5-string/python/main.py
@@ -1,6 +1,11 @@
 from extratorArgumentosUrl import ExtratorArgumentosUrl
 
-url = "https://www.bytebank.com.br/cambio?moedaorigem=real&moedadestino=dolar&valor=700"
+#url = "https://www.bytebank.com.br/cambio?moedaorigem=real&moedadestino=dolar&valor=700"
 #url = ""
 
-print(ExtratorArgumentosUrl.is_valid_url(url))
+url = "moedaorigem=real&moedadestino=dolar"
+argumentos_url = ExtratorArgumentosUrl(url)
+moeda_origem, moeda_destino = argumentos_url.extrai_argumentos()
+
+print(moeda_destino, moeda_origem)
+
