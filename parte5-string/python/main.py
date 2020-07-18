5ff1b484c1616c31ce5cb6de74b351c77d32a3ba
ren-gbh <ren.gbh@gmail.com>
2020-07-18 11:05:26 -0300

    5.1.4 - The find method
    - Include string splitting

--- parte5-string/python/main.py
+++ parte5-string/python/main.py
@@ -1,4 +1,17 @@
+argumento = "moedaorigem=real"
+listaargumentos = argumento.split("=")
+print(listaargumentos)
+
+'''
+argumento = "https://www.bytebank.com.br/cambio?moedaorigem=real"
+index = argumento.find("=")
+substring = argumento[index + 1:]
+print(substring)
+'''
+
+'''
 argumento = "https://www.bytebank.com.br/cambio?moedaorigem=real"
 #............012345678910
 substring = argumento[5:11]
 print(substring)
+'''
