21e7ce5440fec0d6e343685f17322199b98214ff
ren-gbh <ren.gbh@gmail.com>
2020-07-18 11:16:18 -0300

    5.1.8 - Hands on
    - Find "argumentos" text on url

--- parte5-string/python/main.py
+++ parte5-string/python/main.py
@@ -1,7 +1,16 @@
+# Mãos na massa - Encontrando o texto após o caractere "?"
+url = "pagina?argumentos"
+indice = url.find("?")
+print(url[indice + 1:])
+
+# Exemplo com split
+'''
 argumento = "moedaorigem=real"
 listaargumentos = argumento.split("=")
 print(listaargumentos)
+'''
 
+# Exemplo com find
 '''
 argumento = "https://www.bytebank.com.br/cambio?moedaorigem=real"
 index = argumento.find("=")
@@ -9,7 +18,8 @@ substring = argumento[index + 1:]
 print(substring)
 '''
 
-'''
+# Exemplo com fatiamento e indices
+'''  
 argumento = "https://www.bytebank.com.br/cambio?moedaorigem=real"
 #............012345678910
 substring = argumento[5:11]
