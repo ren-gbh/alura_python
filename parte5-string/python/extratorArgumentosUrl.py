ac8032eb438f79dc924fb58085ebc68b44fffb5e
ren-gbh <ren.gbh@gmail.com>
2020-07-19 22:31:31 -0300

    5.3.1 - The replace method

--- parte5-string/python/extratorArgumentosUrl.py
+++ parte5-string/python/extratorArgumentosUrl.py
@@ -14,18 +14,27 @@ class ExtratorArgumentosUrl:
             return False
 
     def extrai_argumentos(self):
-        busca_moeda_origem = "moedaorigem"
-        busca_moeda_destino = "moedadestino"
+        busca_moeda_origem = "moedaorigem="
+        busca_moeda_destino = "moedadestino="
 
         indice_inicial_moeda_origem = self.encontra_indice_inicial(busca_moeda_origem)
         indice_final_moeda_origem = self.url.find("&")
+        moeda_origem = self.url[indice_inicial_moeda_origem:indice_final_moeda_origem]
+
+        if moeda_origem == "moedadestino":
+            self.troca_moeda_origem()
+            indice_inicial_moeda_origem = self.encontra_indice_inicial(busca_moeda_origem)
+            indice_final_moeda_origem = self.url.find("&")
+            moeda_origem = self.url[indice_inicial_moeda_origem:indice_final_moeda_origem]
 
         indice_inicial_moeda_destino = self.encontra_indice_inicial(busca_moeda_destino)
-
-        moeda_origem = self.url[indice_inicial_moeda_origem:indice_final_moeda_origem]
         moeda_destino = self.url[indice_inicial_moeda_destino:]
 
         return moeda_origem, moeda_destino
 
     def encontra_indice_inicial(self, moeda_buscada):
-        return self.url.find(moeda_buscada) + len(moeda_buscada) + 1
+        return self.url.find(moeda_buscada) + len(moeda_buscada)
+
+    def troca_moeda_origem(self):
+        self.url = self.url.replace("moedadestino", "real", 1)
+        print(self.url)
--- parte5-string/python/main.py
+++ parte5-string/python/main.py
@@ -1,6 +1,6 @@
 from extratorArgumentosUrl import ExtratorArgumentosUrl
 
-url = "https://www.bytebank.com.br/cambio?moedaorigem=real&moedadestino=dolar"
+url = "https://www.bytebank.com.br/cambio?moedaorigem=moedadestino&moedadestino=dolar"
 
 argumentos_url = ExtratorArgumentosUrl(url)
 moeda_origem, moeda_destino = argumentos_url.extrai_argumentos()
