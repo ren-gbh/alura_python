616b331ba29055f72df031b2a392946290bb18ad
ren-gbh <ren.gbh@gmail.com>
2020-07-26 13:16:17 -0300

    6.1.4 - List mutabilitity issues

---
+++ parte6-list/aula1/lesson1_4.py
@@ -0,0 +1,13 @@
+def faz_processamento_de_visualizacao(lista = None):
+    if (lista == None):
+        lista = list()
+    print(len(lista))
+    lista.append(13)
+
+idades = [16, 21, 29, 56, 43]
+faz_processamento_de_visualizacao(idades)
+print(idades)
+
+faz_processamento_de_visualizacao()
+faz_processamento_de_visualizacao()
+faz_processamento_de_visualizacao()
